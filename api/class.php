128d48b3b554b03d6087b01459aa1c027388927a
damage99 <p_prostoj@mail.ru>
2017-11-02 20:45:02 +0400

    [convert] Конвертация
    Конвертация валют по статическому курсу

--- api/class.php
+++ api/class.php
@@ -6,38 +6,52 @@ class Converter
    "post"=>'POST аргументы не определены!',
    "get"=>'GET аргументы не определены!',
    "args"=>'Неверные аргументы конвертации!',
-   "valute"=>'Заданные виды валют - не поддерживаются!',
-   "value"=>''
+   "valute"=>'Заданные валюты не поддерживаются, или переводимая валюта с конечной - равны!',
+   "value"=>'Неверный тип значения для перевода!'
    );
+   
+   public function Finish($res="") {
+	   die(json_encode(array("status"=>'success', "value"=>$res))); 
+   }
 
    public function Error($abrerr="") {
+	   echo $this->SE[$abrerr];
        die(json_encode(array("status"=>'error', "description"=>$this->SE[$abrerr])));
    }
    
    public function TryConvert($P) {
-	   if( $this->isVV($from=(string)$P['from']) && $this->isVV($to=(string)$P['to']) ) 
+	   if( $this->is_valute($from=$P['from']) && $this->is_valute($to=$P['to']) && ($from!=$to) ) 
 	   {
-		   if( $this->isVF($value=(double)$P['value']) )
+		   if( is_int($value=(float)$P['value']) || is_float($value) )
 			   $this->doConvert($from,$to,$value);
 		   else
-			   Error('value'); 
+			   $this->Error('value'); 
 	   }
 	   else
-		   Error('valute');
+		   $this->Error('valute');
 		   
    }
    
    private function doConvert($f,$t,$v) {
+	   $vov = $this->LoadVoV($f,$t);
+	   
+	   $this->Finish((($vov[0]*$v)/$vov[1]));
 	   
    }
    
-   
-   private function isVV($s) {
-	   
+   private function is_valute($s) {
+	   return in_array($s,array("usd","rub","eur"));
    }
    
-   private function isVF($d) {
+   private function LoadVov($f,$t) {
+	   $cur = array(
+	   "rub"=>1,
+	   "usd"=>58.3,
+	   "eur"=>68.0
+	   );
 	   
+	   return array($cur[$f],$cur[$t]);
    }
+   
 }
 ?>
